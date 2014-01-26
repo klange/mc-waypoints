372a1a7a1da0d4c8d41cdfd104d26bf62c385289
Kevin Lange <kevin.lange@dakko.us>
2014-01-26 01:44:47 -0800

    awesome script

--- update-symlinks.sh
+++ update-symlinks.sh
@@ -3,6 +3,25 @@
 THIS=`basename "$(pwd)"`
 IP=$1
 
+if [ -n "$IP" ]; then
+    echo "Updating saved IP to $IP"
+    echo -n $IP > .ip
+fi
+if [ -z "$IP" ]; then
+    IP=`cat .ip`
+    echo "Using saved IP $IP"
+fi
+
+echo "Importing worlds from minecraft..."
+pushd .. > /dev/null
+for i in $IP.*.points; do
+    if [ ! -h $i ]; then
+        T=${i//$IP./}
+        mv $i $THIS/$T
+    fi
+done
+popd > /dev/null
+
 echo "Exporting new worlds from git..."
 for i in *.points; do
     pushd .. > /dev/null
